e2366d234cc0725aa8d978aa808e6bdf6ada4df3
samuel forero <samuel.forero@componenteserviex.com>
2023-10-05 09:43:09 -0500

    init firebase project

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'tradicarte-c9274',
+    appId: '1:616088734338:web:bfd31cdec8d33ed3f7429b',
+    storageBucket: 'tradicarte-c9274.appspot.com',
+    apiKey: 'AIzaSyCpN_Y6Z68n-l8AutRRbNieCuS-XRbuhMc',
+    authDomain: 'tradicarte-c9274.firebaseapp.com',
+    messagingSenderId: '616088734338',
+  },
+};
