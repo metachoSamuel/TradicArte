c1a2197404d84040ffd6a5f9186bfc271beaeb3f
samuel forero <samuel.forero@componenteserviex.com>
2023-10-05 10:40:46 -0500

    feature: add register, login, home component and user service, interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,26 @@ import { AppComponent } from './app.component';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth())
+    provideAuth(() => getAuth()),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
