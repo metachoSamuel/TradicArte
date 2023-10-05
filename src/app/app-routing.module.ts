9cfada25bde08998c4bdd03d82a7a244955a1a62
samuel forero <samuel.forero@componenteserviex.com>
2023-10-05 11:06:36 -0500

    feature: register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {RegisterComponent} from "./register/register.component";
 import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent }
 ];
 
